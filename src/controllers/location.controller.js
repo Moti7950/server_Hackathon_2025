6f824ef400148bf55b27b142ddc8e994bfef8559
david teller <ydt009@gmail.com>
2025-09-14 16:25:58 +0300

    endpoint forlocation-working/shayateller/version1

---
+++ src/controllers/location.controller.js
@@ -0,0 +1,6 @@
+import { forceLocationService } from "../../database/index.js"
+export default async function getLocations(req,res) {
+    console.log("hi from location")
+    res.status(200).send(await forceLocationService.getAllForceLocations())
+    
+}
