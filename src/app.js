bf80039e5bc70b2ee914f08765433781e07a51ae
david teller <ydt009@gmail.com>
2025-09-14 16:25:58 +0300

    endpoint forlocation-working/shayateller/version1

--- src/app.js
+++ src/app.js
@@ -1,10 +1,13 @@
 import express from "express";
-import userRoute from "./routes/user.routes"
+import userRoute from "./routes/user.routes.js";
+import  locationRoutes from "./routes/locations.routes.js";
 
 const serverIntelligenceAttack = express();
 
 serverIntelligenceAttack.use(express.json());
 
 serverIntelligenceAttack.use("/users", userRoute);
+//
+serverIntelligenceAttack.use("/locations", locationRoutes);
 
 export default serverIntelligenceAttack;
