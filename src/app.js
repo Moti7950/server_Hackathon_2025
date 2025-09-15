9d8cce4f663992e7e0b144a151eb31c6fa905517
yitzchakdamen <yitzchakdamen@gmail.com>
2025-09-15 14:12:00 +0300

    chancg database to supabase

--- src/app.js
+++ src/app.js
@@ -1,8 +1,12 @@
 import express from "express";
 import userRoute from "./routes/user.routes.js";
 import  locationRoutes from "./routes/locations.routes.js";
+import locationsDB from '../lib/supabase.js'
+
 
 const serverIntelligenceAttack = express();
+
+
 
 serverIntelligenceAttack.use(express.json());
 
