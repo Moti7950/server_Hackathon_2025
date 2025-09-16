b34048f395e57a8b97a0b2f9cbdb6af3a56b0245
david teller <ydt009@gmail.com>
2025-09-16 12:02:49 +0300

    add new suspiciousPoints endpoint/route - controller-dal

--- src/app.js
+++ src/app.js
@@ -1,17 +1,30 @@
 import express from "express";
 import userRoute from "./routes/user.routes.js";
 import locationRoutes from "./routes/locations.routes.js";
+import SuspiciousRouter from "./routes/suspicious.routes.js";
 import cors from "cors";
 import locationsDB from "../lib/supabase.js";
 
 const serverIntelligenceAttack = express();
 
 serverIntelligenceAttack.use(cors());
-
 serverIntelligenceAttack.use(express.json());
 
+
+// Middleware to log request method and URL
+serverIntelligenceAttack.use((req, res, next) => {
+  console.log(`${req.method} ${req.url}`);
+  next();
+});
+
+//users routes
 serverIntelligenceAttack.use("/users", userRoute);
 
+//locations routes
 serverIntelligenceAttack.use("/locations", locationRoutes);
+
+//suspicious points routes
+serverIntelligenceAttack.use("/suspiciousPoints", SuspiciousRouter);
+
 
 export default serverIntelligenceAttack;
